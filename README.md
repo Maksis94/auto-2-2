ea3d19c0360465b80f953daaa26d92b674e94fcc
Maksis94 <palchenkov94@gmail.com>
2023-10-23 16:44:45 +0300

    исправление теста

---
+++ README.md
@@ -0,0 +1,3 @@
+# auto2
+
+[![Build status](https://ci.appveyor.com/api/projects/status/78n7as225t91vvjw?svg=true)](https://ci.appveyor.com/project/Maksis94/auto-2-2)
--- src/test/java/ru/netology/PostmanTest.java
+++ src/test/java/ru/netology/PostmanTest.java
@@ -16,6 +16,6 @@ class PostmanTest {
                 .post("/post")
             .then()
                 .statusCode(200)
-                .body("data", equalTo("some data 1"));
+                .body("data", equalTo("some data"));
     }
 }
